b5801f4a6800a7b8c1828f271aaf3f0ee2abfad7
niqbal1989 <nadia.iqbal963@gmail.com>
2019-02-19 13:28:47 -0500

    making sure scraping works on a single page

--- movie_scraping.py
+++ movie_scraping.py
@@ -1,5 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
+import pandas as pd
 titles = []
 year = []
 star = []
@@ -10,19 +11,24 @@ response = get(url)
 hsoup = BeautifulSoup(response.text, 'html.parser')
 movies_info = hsoup.find_all('div', class_ = 'lister-item mode-advanced')
 for movie in movies_info:
-	movie_name = movie1.h3.a.text
-	movie_year = movie1.h3.find('span', class_ = 'lister-item-year text-muted unbold')
+	movie_name = movie.h3.a.text
+	movie_year = movie.h3.find('span', class_ = 'lister-item-year text-muted unbold')
 	movie_year = movie_year.text
-	info=movie1.find_all('p', class_ = "")
+	info=movie.find_all('p', class_ = "")
 	dir_stars = info[1].text
 	idx = dir_stars.find("Stars")
 	stars = dir_stars[idx+7:len(dir_stars)-1]
 	stars = stars.replace(', ','')
-	stars = stars.splitlines
+	stars = stars.split('\n')
 	for i in stars:
 		titles.append(movie_name)
 		year.append(movie_year)
-		stars.append(i)
+		star.append(i)
+
+
+movie_stars_df = pd.DataFrame({'title': titles,
+				'year': year,
+				'stars': star})
 
 
 url = 'https://www.boxofficemojo.com/yearly/chart/?yr=2009&view=releasedate&view2=domestic&sort=opengross&order=DESC&&p=.htm'
@@ -30,12 +36,12 @@ response = get(url)
 hsoup2 = BeautifulSoup(response.text, 'html.parser')
 table = hsoup2.find_all('b')
 movies_BO = table[9:209]
-titles = []
+titles1 = []
 OWBO = []
 for i,val in enumerate(movies_BO):
-	if i%0 == 0:
+	if i%2 == 0:
 		title = val.a.text
-		titles.append(title)
+		titles1.append(title)
 	else:
 		x = str(val)
 		x = x[4:len(x)-4]
@@ -43,6 +49,9 @@ for i,val in enumerate(movies_BO):
 		x = ''.join(x)
 		OWBO.append(int(x))
 		
+movie_OWBO = pd.DataFrame({'title': titles1,
+				'OWBO': OWBO})
+
 		
 	
 
