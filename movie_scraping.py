5f12ecf3faef456103d36602cb69426105e44665
niqbal1989 <29986975+niqbal1989@users.noreply.github.com>
2019-02-19 18:20:37 -0500

    creating automation for scraping necessary data from both websites

--- movie_scraping.py
+++ movie_scraping.py
@@ -42,24 +42,29 @@ movie_stars_df = pd.DataFrame({'title': titles,
 				'year': year,
 				'stars': star})
 
-
-url = 'https://www.boxofficemojo.com/yearly/chart/?yr=2009&view=releasedate&view2=domestic&sort=opengross&order=DESC&&p=.htm'
-response = get(url)
-hsoup2 = BeautifulSoup(response.text, 'html.parser')
-table = hsoup2.find_all('b')
-movies_BO = table[9:209]
-titles1 = []
-OWBO = []
-for i,val in enumerate(movies_BO):
-	if i%2 == 0:
-		title = val.a.text
-		titles1.append(title)
-	else:
-		x = str(val)
-		x = x[4:len(x)-4]
-		x = x.split(',')
-		x = ''.join(x)
-		OWBO.append(int(x))
+bom_year_url = [str(i) for i in range(2009, 2018)]
+for year_url in bom_year_url:
+	for x in iter1:
+		if x == 1:
+			url = 'https://www.boxofficemojo.com/yearly/chart/?yr='+ year_url+'&view=releasedate&view2=domestic&sort=opengross&order=DESC&&p=.htm'
+		if x == 2:
+			url = 'https://www.boxofficemojo.com/yearly/chart/?page='+ str(x)+'&view=releasedate&view2=domestic&yr='+ year_url +'&sort=opengross&p=.htm'
+		response = get(url)
+		hsoup2 = BeautifulSoup(response.text, 'html.parser')
+		table = hsoup2.find_all('b')
+		movies_BO = table[9:209]
+		titles1 = []
+		OWBO = []
+		for i,val in enumerate(movies_BO):
+			if i%2 == 0:
+				title = val.a.text
+				titles1.append(title)
+			else:
+				x = str(val)
+				x = x[4:len(x)-4]
+				x = x.split(',')
+				x = ''.join(x)
+				OWBO.append(int(x))
 		
 movie_OWBO = pd.DataFrame({'title': titles1,
 				'OWBO': OWBO})
